f69668d58b55dd31c50885f04888b5d625bb9cde
Oliwier Kasprzak <oliwier.kasprzak@icloud.com>
2023-01-12 11:40:56 +0100

    Add files via upload

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Oliwier Kasprzak on 11/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
