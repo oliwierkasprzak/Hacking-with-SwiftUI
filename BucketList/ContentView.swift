f2a32fd97e92fcf0961f672de4addea5ac3552bb
Oliwier Kaspprzak <oliwier.kasprzak00gmail.com>
2023-03-07 20:28:11 +0100

    Clean state for tomorrow's project

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -6,40 +6,10 @@
 //
 
 import SwiftUI
-
-enum LoadingState {
-    case loading, success, failed
-}
-
-struct LoadingView: View {
-    var body: some View {
-        Text("Loading...")
-    }
-}
-
-struct SuccessView: View {
-    var body: some View {
-        Text("Success!")
-    }
-}
-
-struct FailedView: View {
-    var body: some View {
-        Text("Failed.")
-    }
-}
-
 struct ContentView: View {
-    var loadingState = LoadingState.loading
-
+    
     var body: some View {
-        if loadingState == .loading {
-            LoadingView()
-        } else if loadingState == .success {
-            SuccessView()
-        } else if loadingState == .failed {
-            FailedView()
-        }
+        Text("Hello, World!")
     }
 }
 
