6e7fa3db4527846a440cac2ae84b099f8f5d20b0
Oliwier Kaspprzak <oliwier.kasprzak00gmail.com>
2023-03-13 20:17:40 +0100

    Initial Commit

---
+++ Accesibility/AccesibilityApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccesibilityApp.swift
+//  Accesibility
+//
+//  Created by Oliwier Kasprzak on 13/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct AccesibilityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
