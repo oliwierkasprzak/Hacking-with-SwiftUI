a9b681d83240833ef7c53bd1a2e9d383136da561
Oliwier Kaspprzak <oliwier.kasprzak00gmail.com>
2023-03-13 22:38:40 +0100

    Accessibility introduction

--- Accesibility/ContentView.swift
+++ Accesibility/ContentView.swift
@@ -8,14 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var value = 10
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Value: \(value)")
+            
+            Button("Increment") {
+                value += 1
+            }
+            
+            Button("Decrement") {
+                value -= 1
+            }
         }
-        .padding()
+        .accessibilityElement()
+        .accessibilityLabel("Value")
+        .accessibilityValue(String(value))
+        .accessibilityAdjustableAction { direction in
+            switch direction {
+            case .increment:
+                value += 1
+                
+            case .decrement:
+                value -= 1
+            default:
+                print("not handled")
+            }
+        }
     }
 }
 
