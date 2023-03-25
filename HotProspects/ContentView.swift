db48a98f63d10d4544c4752d62fa37df919eddbf
Oliwier Kaspprzak <oliwier.kasprzak00gmail.com>
2023-03-25 21:37:08 +0100

    @EnvironmentObject introuduction

--- HotProspects/ContentView.swift
+++ HotProspects/ContentView.swift
@@ -7,23 +7,34 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, github!")
-                .padding()
-            
-        }
-        
-        Text("Hello")
-            .padding()
-    }
-        
+@MainActor class User: ObservableObject {
+    @Published var name = "The Weeknd"
 }
 
+struct DisplayView: View {
+    @EnvironmentObject var user: User
+    var body: some View {
+        Text(user.name)
+    }
+}
+
+struct EditView: View {
+    @EnvironmentObject var user: User
+    var body: some View {
+        TextField("Name", text: $user.name)
+    }
+}
+struct ContentView: View {
+    @StateObject var user = User()
+    var body: some View {
+        VStack {
+            EditView()
+            DisplayView()
+        }
+        .environmentObject(user)
+        
+    }
+}
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
