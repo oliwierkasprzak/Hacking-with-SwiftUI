590e0c3e75c7126385ea999aa65f595558c9e5e1
Oliwier Kasprzak <oliwier.kasprzak@icloud.com>
2023-03-31 15:29:08 +0200

    Day 86 finished.

---
+++ Flashzilla/Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Oliwier Kasprzak on 31/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
