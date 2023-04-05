2f50a9782b333cd5c26d739ddca93ae8da3a9e4b
Oliwier Kasprzak <oliwier.kasprzak@icloud.com>
2023-04-05 20:30:57 +0200

    Day 92 finished.

---
+++ LayoutAndGeometry/LayoutAndGeometry/LayoutAndGeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutAndGeometryApp.swift
+//  LayoutAndGeometry
+//
+//  Created by Oliwier Kasprzak on 04/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct LayoutAndGeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
