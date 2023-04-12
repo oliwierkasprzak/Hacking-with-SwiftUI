728ddd7dc824b26d049267a89100f862a9dd4da3
Oliwier Kasprzak <oliwier.kasprzak@icloud.com>
2023-04-12 12:49:42 +0200

    Day 98 finished.

---
+++ SnowSeeker/SnowSeeker/Resort.swift
@@ -0,0 +1,29 @@
+//
+//  Resort.swift
+//  SnowSeeker
+//
+//  Created by Oliwier Kasprzak on 11/04/2023.
+//
+
+import Foundation
+
+struct Resort: Codable, Identifiable {
+    var id: String
+    var name: String
+    var country: String
+    var description: String
+    var imageCredit: String
+    var price: Int
+    var size: Int
+    var snowDepth: Int
+    var elevation: Int
+    var runs: Int
+    var facilities: [String]
+    
+    var facilityType: [Facility] {
+        facilities.map(Facility.init)
+    }
+    
+    static let allResorts: [Resort] = Bundle.main.decode("resorts.json")
+    static let example = allResorts[0]
+}
---
+++ SnowSeeker/SnowSeeker/ResortsDetailView.swift
@@ -0,0 +1,52 @@
+//
+//  ResortsDetailView.swift
+//  SnowSeeker
+//
+//  Created by Oliwier Kasprzak on 11/04/2023.
+//
+
+import SwiftUI
+
+struct ResortsDetailView: View {
+    let resort: Resort
+    
+    var size: String {
+        switch resort.size {
+        case 1:
+            return "Small"
+        case 2:
+            return "Average"
+        default:
+            return "Large"
+        }
+    }
+    
+    var price: String {
+        String(repeating: "$", count: resort.price)
+    }
+    
+    var body: some View {
+        Group {
+            VStack {
+                Text("Size")
+                    .font(.caption.bold())
+                Text(size)
+                    .font(.title3)
+            }
+            
+            VStack {
+                Text("Price")
+                    .font(.caption.bold())
+                Text(price)
+                    .font(.title3)
+            }
+        }
+        .frame(maxWidth: .infinity)
+    }
+}
+
+struct ResortsDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        ResortsDetailView(resort: Resort.example)
+    }
+}
