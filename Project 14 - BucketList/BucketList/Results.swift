115eb1bbcd60ed8e4d8580d02002c7a136066d3d
Oliwier Kasprzak <oliwier.kasprzak@icloud.com>
2023-04-24 09:33:59 +0200

    Challenges finished.

---
+++ Project 14 - BucketList/BucketList/Results.swift
@@ -0,0 +1,33 @@
+//
+//  Results.swift
+//  BucketList
+//
+//  Created by Oliwier Kasprzak on 10/03/2023.
+//
+
+import Foundation
+
+import Foundation
+
+struct Result: Codable {
+    let query: Query
+}
+
+struct Query: Codable {
+    let pages: [Int: Page]
+}
+
+struct Page: Codable, Comparable {
+    let pageid: Int
+    let title: String
+    let terms: [String: [String]]?
+
+    var description: String {
+        terms?["description"]?.first ?? "No further information"
+    }
+
+    static func <(lhs: Page, rhs: Page) -> Bool {
+        lhs.title < rhs.title
+    }
+}
+
